4bdfabb8be509cc10f123c2e3794651e1c76a8ce
Michael <michael.toepfer187@gmail.com>
2018-01-15 16:42:20 +0100

    update but still inlinestylechanges to do

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,64 @@
+
+        function openNav() {
+            $("#mySidenav").addClass("visible")
+            $("#mySection").addClass("Section")
+            $(".navigation").addClass("navi")
+
+        }
+
+        function closeNav() {
+            $("#mySidenav").removeClass("visible")
+            $("#mySection").removeClass("Section")
+            $(".navigation").removeClass("navi")
+        }
+
+
+      
+
+
+$(function() {
+        $('#someTab').tab('show')
+
+        $('a.nav-link').on("click",function(){
+          closeNav()
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+// this.bottomNav = function() {
+//        $('nav ul li, #register_btn, #next_game, #register_btn_top, #contact_btn').on('click', function() {
+//            $('section').hide();
+//            var content = $(this).data('content');
+//            $('#' + content).show();
+//        });
+//    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  document.addEventListener('DOMContentLoaded', function() {
         // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
         // // The Firebase SDK is initialized and available here!
